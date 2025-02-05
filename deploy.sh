5ebe52794e327531357647d81d69b63bfb3e01f2
shubhashreet2004 <135111992+shubhashreet2004@users.noreply.github.com>
2025-02-05 11:36:51 +0530

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+    echo hi123
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u shubhashree1311 -p shubha@1311
+    docker tag test shubhashree1311/task_2
+    docker push shubhashree1311/task_2
+    
